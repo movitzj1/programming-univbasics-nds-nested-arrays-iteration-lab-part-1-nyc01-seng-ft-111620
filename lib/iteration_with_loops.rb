5d886e6a3830a34c40e7cba183c79d91cf8ca617
Justin Movitz <movitzj1@montclair.edu>
2020-10-23 19:28:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_even_values(src)
   while count < src.count do
     inner_count = 0
     while inner_count < src[count].count do
-      if src[inner_count] % 2 == 0
+      if src[count][inner_count] % 2 == 0
       p src[count][inner_count]
       inner_count += 1
       end
